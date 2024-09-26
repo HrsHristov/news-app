334b052dc405f333c0533cc29659aab783a94a60
Hristo Hristov <hrshristov@gmail.com>
2024-09-26 19:41:43 +0300

    Add Parallel routes for Archive/Latest

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,15 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news";
+
+const LatestNewsPage = () => {
+    const latest = getLatestNews();
+
+    return (
+        <>
+            <h2>Latest News</h2>
+            <NewsList news={latest} />
+        </>
+    );
+};
+
+export default LatestNewsPage;
--- app/news/page.js
+++ app/news/page.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import NewsList from "@/components/news-list";
 
 import { DUMMY_NEWS } from "@/dummy-news";
 
@@ -6,19 +6,7 @@ const NewsPage = () => {
     return (
         <>
             <h1>NewsPage</h1>
-            <ul className="news-list">
-                {DUMMY_NEWS.map((newsItem) => (
-                    <li key={newsItem.id}>
-                        <Link href={`/news/${newsItem.slug}`}>
-                            <img
-                                src={`/images/news/${newsItem.image}`}
-                                alt={newsItem.title}
-                            />
-                            <span>{newsItem.title}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <NewsList news={DUMMY_NEWS} />
         </>
     );
 };
