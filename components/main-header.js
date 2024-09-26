49fc328dabc7b54cbec2ef1d2d4b768500fb0ab1
Hristo Hristov <hrshristov@gmail.com>
2024-09-26 21:14:47 +0300

    Add nav-link component

--- components/main-header.js
+++ components/main-header.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import NavLink from "./nav-link";
+
 const MainHeader = () => {
     return (
         <header id="main-header">
@@ -9,7 +11,10 @@ const MainHeader = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link href="/news">News</Link>
+                        <NavLink href="/news">News</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/archive">Archive</NavLink>
                     </li>
                 </ul>
             </nav>
