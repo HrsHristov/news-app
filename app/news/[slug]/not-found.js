1023fae5292b7758d27500989f587100abc18487
Hristo Hristov <hrshristov@gmail.com>
2024-09-26 17:53:07 +0300

    Add not-found errors

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,13 @@
+const NewsNotFoundPage = () => {
+    return (
+        <div id="error">
+            <h1>Not Found!</h1>
+            <p>
+                Unfortunately, we could not find the requested resource could
+                not be found!
+            </p>
+        </div>
+    );
+};
+
+export default NewsNotFoundPage;
