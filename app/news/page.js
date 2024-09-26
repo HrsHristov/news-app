eda7ea75afc1416e3ebfd73a1227dd08578661e2
Hristo Hristov <hrshristov@gmail.com>
2024-09-26 17:26:48 +0300

    Add news routes

---
+++ app/news/page.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const NewsPage = () => {
+    return (
+        <>
+            <h1>NewsPage</h1>
+            <ul>
+                <li>
+                    <Link href="/news/first-news">First News Items</Link>
+                </li>
+                <li>
+                    <Link href="/news/second-news">Second News Items</Link>
+                </li>
+                <li>
+                    <Link href="/news/third-news">Third News Items</Link>
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default NewsPage;
